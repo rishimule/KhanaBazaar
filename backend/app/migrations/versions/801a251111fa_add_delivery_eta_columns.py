e7b15023d0a8340199e07bb77996077db69c4009
Khana Bazaar Developer <developer@khanabazaar.local>
2026-05-30 17:16:17 -0400

    chore(seller-eta): fix migration import ordering

--- backend/app/migrations/versions/801a251111fa_add_delivery_eta_columns.py
+++ backend/app/migrations/versions/801a251111fa_add_delivery_eta_columns.py
@@ -9,8 +9,8 @@ Create Date: 2026-05-30 16:44:03.655787
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '801a251111fa'
