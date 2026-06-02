1e53164d97b4f63aeeb33c6483790a15d64a7663
Khana Bazaar Developer <developer@khanabazaar.local>
2026-06-02 11:20:12 -0400

    style(migration): sort imports in delivery-otp migration

--- backend/app/migrations/versions/1a82f642a321_delivery_otp_columns.py
+++ backend/app/migrations/versions/1a82f642a321_delivery_otp_columns.py
@@ -9,8 +9,8 @@ Create Date: 2026-06-02 10:21:47.473215
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '1a82f642a321'
